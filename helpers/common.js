d45af0d124cb9dd75914f4f2fe9ff747fd9eca91
Chizycodes <okwudirejoy@gmail.com>
2024-09-28 10:55:00 +0100

    Image grid and masonry layout

--- helpers/common.js
+++ helpers/common.js
@@ -9,3 +9,29 @@ export const wp = (percentage) => {
 export const hp = (percentage) => {
 	return (percentage * deviceHeight) / 100;
 };
+
+export const getColumnCount = () => {
+	if (deviceWidth >= 1024) {
+		// desktop
+		return 4;
+	} else if (deviceHeight >= 768) {
+		// tablet
+		return 3;
+	} else {
+		// mobile
+		return 2;
+	}
+};
+
+export const getImageSize = (height, width) => {
+	if (width > height) {
+		// landscape
+		return 250;
+	} else if (width < height) {
+		// portrait
+		return 300;
+	} else {
+		// square
+		return 200;
+	}
+};
