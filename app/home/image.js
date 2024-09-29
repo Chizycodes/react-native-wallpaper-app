f87869fafdac3fdc506a9f3916e7fb654adfda23
Chizycodes <okwudirejoy@gmail.com>
2024-09-29 13:10:48 +0100

    Share and download actions

--- app/home/image.js
+++ app/home/image.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, Platform, ActivityIndicator, Pressable } from "react-native";
+import { View, Text, StyleSheet, Button, Platform, ActivityIndicator, Pressable, Alert } from "react-native";
 import React, { useState } from "react";
 import { BlurView } from "expo-blur";
 import { hp, wp } from "../../helpers/common";
@@ -7,12 +7,17 @@ import { Image } from "expo-image";
 import { theme } from "../../constants/theme";
 import { Entypo, Octicons } from "@expo/vector-icons";
 import Animated, { FadeInDown } from "react-native-reanimated";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
 
 const image = () => {
 	const router = useRouter();
 	const item = useLocalSearchParams();
 	const [status, setStatus] = useState("loading");
-	let url = item?.webformatURL;
+	let uri = item?.webformatURL;
+	const fileName = item?.previewURL?.split("/").pop();
+	const imageUrl = uri;
+	const filePath = `${FileSystem.documentDirectory}${fileName}`;
 
 	const getSize = () => {
 		const aspectRatio = item?.imageWidth / item?.imageHeight;
@@ -35,12 +40,40 @@ const image = () => {
 		setStatus("");
 	};
 
+	const handleDownloadImage = async () => {
+		setStatus("downloading");
+		let uri = await downloadFile();
+		if (uri) {
+			console.log("Downloaded");
+		}
+	};
+
+	const handleShareImage = async () => {
+		setStatus("sharing");
+		let uri = await downloadFile();
+		if (uri) {
+			await Sharing.shareAsync(uri);
+		}
+	};
+
+	const downloadFile = async () => {
+		try {
+			const { uri } = await FileSystem.downloadAsync(imageUrl, filePath);
+			setStatus("");
+			return uri;
+		} catch (error) {
+			setStatus("");
+			Alert.alert("Image", error.message);
+			return null;
+		}
+	};
+
 	return (
 		<BlurView style={styles.container} tint="dark" intensity={60}>
 			<View style={getSize()}>
 				<View style={styles.loading}>{status === "loading" && <ActivityIndicator size="large" color="white" />}</View>
 
-				<Image transition={100} style={[styles.image, getSize()]} source={url} onLoad={onLoad} />
+				<Image transition={100} style={[styles.image, getSize()]} source={uri} onLoad={onLoad} />
 			</View>
 
 			<View style={styles.buttons}>
@@ -51,15 +84,27 @@ const image = () => {
 				</Animated.View>
 
 				<Animated.View entering={FadeInDown.springify().delay(100)}>
-					<Pressable style={styles.button}>
-						<Octicons name="download" size={24} color="white" />
-					</Pressable>
+					{status === "downloading" ? (
+						<View style={styles.button}>
+							<ActivityIndicator size="small" color="white" />
+						</View>
+					) : (
+						<Pressable style={styles.button} onPress={handleDownloadImage}>
+							<Octicons name="download" size={24} color="white" />
+						</Pressable>
+					)}
 				</Animated.View>
 
 				<Animated.View entering={FadeInDown.springify().delay(200)}>
-					<Pressable style={styles.button}>
-						<Entypo name="share" size={22} color="white" />
-					</Pressable>
+					{status === "sharing" ? (
+						<View style={styles.button}>
+							<ActivityIndicator size="small" color="white" />
+						</View>
+					) : (
+						<Pressable style={styles.button} onPress={handleShareImage}>
+							<Entypo name="share" size={22} color="white" />
+						</Pressable>
+					)}
 				</Animated.View>
 			</View>
 		</BlurView>
